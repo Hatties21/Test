f24833569dfd2799b6158d8f54a73cfa415fbf3f
Hatties21 <vuthanhnam149@gmail.com>
2025-06-28 00:21:01 +0700

    sua loi thang toan

--- client/src/pages/Library.jsx
+++ client/src/pages/Library.jsx
@@ -2,18 +2,24 @@ import { Box } from "@mui/material";
 
 const Library = () => {
   return (
-    <div className="library">
-      <h1>Olds </h1>
-      {/* Sẽ thêm danh sách bài hát sau */}
+    <div className="library" style={{ position: "relative" }}>
+      {/* Floating Box */}
       <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: 3,
-        width: "100%",
-      }}
-    ></Box>
+        sx={{
+          position: "absolute",
+          top: "50px",
+          right: "50px",
+          width: 300,
+          padding: 3,
+          bgcolor: "background.paper",
+          boxShadow: 3,
+          borderRadius: 2,
+          zIndex: 1000,
+        }}
+      >
+        {/* Nội dung floating box */}
+        <p>Hello</p>
+      </Box>
     </div>
   );
 };
