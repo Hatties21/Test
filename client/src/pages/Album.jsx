d14c2c340bf1463aa1f4285a20dfde2129deb4ae
Toan Nguyen <giautenbantoan123@gmail.com>
2025-06-27 23:24:11 +0700

    test

--- client/src/pages/Album.jsx
+++ client/src/pages/Album.jsx
@@ -1,8 +1,17 @@
 const Album = () => {
   return (
     <div className="album">
-      <h1> </h1>
+      <h1> Nega </h1>
       {/* Sẽ thêm danh sách bài hát sau */}
+      <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: 3,
+        width: "100%",
+      }}
+    ></Box>
     </div>
   );
 };
