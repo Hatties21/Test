ade9a61c8e7f77d7a854519ff46640309ea20074
Toan Nguyen <giautenbantoan123@gmail.com>
2025-06-28 11:34:24 +0700

    1

--- client/src/pages/Album.jsx
+++ client/src/pages/Album.jsx
@@ -1,9 +1,9 @@
 const Album = () => {
   return (
     <div className="album">
-      <h1> </h1>
+      <h1>Toan </h1>
       {/* Sẽ thêm danh sách bài hát sau */}
-      
+      <h2>Xung dot</h2>
     </div>
     
   );
